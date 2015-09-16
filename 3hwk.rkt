929a48ab9f12b0697a85e853c6ef2250f0e11e77
SkiingxMoose <jodesmond@live.com>
2015-09-15 22:43:10 -0400

    added four high level functions

--- 3hwk.rkt
+++ 3hwk.rkt
@@ -105,6 +105,60 @@ dir -> lod -> dir -> lod -> dir -> lod -> dir end -> lof -> lof -> lof
 |#
 
 
+;; For use in test cases for the following four functions:
+(define FN-1 (make-file 'a 5 0))
+(define FN-2 (make-file 'a 5 1))
+(define FS-1 (make-file 'a 5 's))
+(define NUMBERS1 (make-dir 'numbers1 empty (list FN-1 FN-1)))
+(define NUMBERS (make-dir 'numbers (list NUMBERS1 NUMBERS1) (list FN-1 FN-1)))
+(define NUMBERS-21 (make-dir 'numbers1 empty (list FN-2 FN-2)))
+(define NUMBERS-2 (make-dir 'numbers (list NUMBERS-21 NUMBERS-21) (list FN-2 FN-2))) 
+
+(define FILTERNUM (lambda (lof) (filter (lambda (f) (number? (file-content f))) lof)))
+(define MIXED1 (make-dir 'm1 empty (list FN-1 FN-2 FS-1)))
+(define MIXED-21 (make-dir 'm1 empty (list FN-1 FN-2)))
+(define MIXED (make-dir 'm (list MIXED1 MIXED1) (list FN-1)))
+(define MIXED-2 (make-dir 'm (list MIXED-21 MIXED-21) (list FN-1)))
+
+;; =============High Level Functions==============
+;; apply-dir: (a-dir, (list[files] -> x) -> a-dir)
+;; Applies the given lof-fun to every single lof
+;; throughout the entire file system
+(define (apply-dir a-dir lof-fun)
+  (cond [(empty? a-dir) empty]
+        [else (make-dir (dir-name a-dir) (apply-lod (dir-dirs a-dir) lof-fun) (lof-fun (dir-files a-dir)))]))
+;; apply-lod: (a-lod, (list[files]-> x) -> a-lod)
+;; Applies the given lof-fun to every single lof
+;; in every directory in the list, and within all
+;; subdirectories/throughout the entire file system.
+(define (apply-lod a-lod lof-fun)
+  (cond [(empty? a-lod) empty]
+        [else (cons (apply-dir (first a-lod) lof-fun) (apply-lod (rest a-lod) lof-fun))]))
+
+;; Test Cases:
+(check-expect (apply-dir NUMBERS (lambda (lof) (map (lambda (f) (make-file (file-name f) (file-size f) (+ 1 (file-content f)))) lof))) NUMBERS-2)
+(check-expect (apply-dir MIXED FILTERNUM) MIXED-2)
+
+;; filter-dir: (a-dir, (file->boolean) -> a-dir)
+;; Returns a directory with all files removed
+;; or filtered out that cause the boolean test to
+;; return false.
+(define (filter-dir a-dir file-cond)
+  (apply-dir a-dir (lambda (lof) (filter file-cond lof))))
+(check-expect (filter-dir MIXED (lambda (f) (number? (file-content f)))) MIXED-2)
+
+;; map-dir (a-dir, (file->file) -> a-dir)
+;; Returns a directory with the given function
+;; applied to every single file in the entire
+;; file system.
+(define (map-dir a-dir file-fun)
+  (apply-dir a-dir (lambda (lof) (map file-fun lof))))
+;; Test Cases
+(check-expect (map-dir NUMBERS (lambda (f) (make-file (file-name f) (file-size f) (+ 1 (file-content f))))) NUMBERS-2)
+
+
+
+
 
 (define LOF1 (list (make-file '0 0 empty)))
 (define LOF2 (list (make-file '0 0 empty) 

50346eb2439ae1b60b76e15e0f7800957a63a5bb
SkiingxMoose <jodesmond@live.com>
2015-09-21 15:55:27 -0400

    tiny commit, just getting started

--- 4hwk.rkt
+++ 4hwk.rkt
@@ -2,3 +2,10 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 4hwk) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")))))
 ;; Josh Desmond Saahil
+
+;; A cmd is either
+;; - (make askquestion ( -> question)
+;; - (make testcond (??? -> boolean) list[cmd] list[cmd])
+(define-struct askquestion (question))
+(define-struct testcond (cond-question failed-cond passed-cond)
+

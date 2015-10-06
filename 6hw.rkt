1c1434c29156e72705eaa3d49c72743711215ea7
SkiingxMoose <jodesmond@live.com>
2015-10-06 19:24:28 -0400

    Changed around formatting/comments

--- 6hw.rkt
+++ 6hw.rkt
@@ -1,65 +1,55 @@
 ;; Saahil claypool & Josh Desmond
 ;; Homework 6
 
-
-;;struct verion
-#|
-;; A dillo is a (make-dillo number boolean)
-(define-struct dillo (length dead?))
-
-;; longer-than? : dillo number -> boolean
-;; is dillo longer than given length
-(define (longer-than? adillo len)
-  (> (dillo-length adillo) len))
-
-;; run-over : dillo -> dillo
-;; return dead dillo one unit longer than given dillo
-(define (run-over adillo)
-  (make-dillo (+ (dillo-length adillo) 1) true))
-|#
-
-
-
-
-
-
-
-
+;; Macro for class
 (define-syntax class
   (syntax-rules (initvars method)
-    [(class (initvars var ...)
+    [(class (initvars var ...) ;; class with methods & variables
        (method funName args fun) ...)
      (lambda (var ...)
        (lambda (message)
          (cond [(symbol=? message 'funName)
                 (lambda args fun)] ...)))]
-    [(class (initvars var ...))
+    [(class (initvars var ...)) ;; no methods
      (lamda (var ...))]
-    [(class (method funName args fun) ...)
+    [(class (method funName args fun) ...) ;; no variables
      (lambda ()
        (lambda (message)
          (cond [(sybmol=? message 'funName)
                 (lambda args fun)] ...)))]))
 
+;; Macro for send
 (define-syntax send
   (syntax-rules ()
     [(send obj message arg)
      ((obj 'message) arg)]
-    [(send obj message)
+    [(send obj message) ;; methods without arguments
      ((obj 'message))]))
 
 
-;; Dillo-class
+;; Dillo-class, an example class using our notation.
 (define dillo-class
   (class (initvars length dead?)
          (method longer-than? (len) (> length len))
          (method run-over () (dillo-class (+ length 1) true))))
 
+;; dillo-class is converted into:
+#|
+(define make-dillo-obj
+  (lambda (length dead?)
+    (lambda (message)
+      (cond [(symbol=? message 'longer-than?) 
+             (lambda (len) (> length len))]
+            [(symbol=? message 'run-over) 
+             (lambda () (make-dillo-obj (+ length 1) true))]))))
+|#
 
+;; Example class without methods
 (define classNoMethod
   (class (initvars length dead?)
          ))
 
+;; Example class without variables
 (define classNoVars
   (class 
          (method longer-than? (len) (> length len))
@@ -73,51 +63,11 @@
 (define d4 (send d3 run-over))
 (send d4 longer-than? 5)
 
-
+;; These are converted into:
 #|
-(define d3 (dillo-class 5 false))
-(send d3 longer-than? 6)
-(send d3 longer-than? 5)
-(define d4 (send d3 run-over))
-(send d4 longer-than? 5)
-
-|#
-
-
-
-                 
-#|
-
-(define d3 (dillo-class 5 false))
-(send d3 longer-than? 6)
-(send d3 longer-than? 5)
-(define d4 (send d3 run-over))
-(send d4 longer-than? 5)
-|#
-
-
-;; Part One 
-
-
-(define make-dillo-obj
-  (lambda (length dead?)
-    (lambda (message)
-      (cond [(symbol=? message 'longer-than?) 
-             (lambda (len) (> length len))]
-            [(symbol=? message 'run-over) 
-             (lambda () (make-dillo-obj (+ length 1) true))]))))
-
 (define d1 (make-dillo-obj 5 false))
 ((d1 'longer-than?) 6)
 ((d1 'longer-than?) 5)
 (define d2 ((d1 'run-over)))
 ((d2 'longer-than?) 5)
-
-
-
-
-(define dillo-class
-  (class (initvars length dead?)
-         (method longer-than? (len) (> length len))
-         (method run-over () (dillo-class (+ length 1) true))))
-
+|#
